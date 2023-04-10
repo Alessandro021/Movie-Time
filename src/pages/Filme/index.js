efa4554c058836c49d5f8e0d16ff3c50f2f25e59
alessandro <alessandrodamata6@gmail.com>
2023-04-09 21:30:33 -0300

    configurado a pagina de filmes

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,7 +1,58 @@
+import { useState, useLayoutEffect } from "react"
+import { useParams } from "react-router-dom"
+import api from '../../services/api.js'
+
+import './filme.css'
+
 export default function Filme(){
+    const { id } = useParams();
+    const [filme, setFilme] = useState({});
+    const [loading, setLoading] = useState(true)
+
+    useLayoutEffect(() => {
+        async function loadFilme(){
+            await api.get(`/movie/${id}`, {
+                params:{
+                    api_key: "2f80d2c6cee2d978397b2ef6c5ba08a0",
+                    language: "pt-BR",
+                }
+            })
+            .then((response)=>{
+                setFilme(response.data)
+                setLoading(false)
+            })
+            .catch(()=>{
+                console.log("filme não encontrado")
+            })
+        }
+
+        loadFilme()
+
+        return () => {
+            console.log("componente foi desmontado")
+        }
+    },[])
     return(
-        <div>
-            <h1>Pagina de detalhe de Filme</h1>
-        </div>
+        !loading ? (
+            <div className="filme-info">
+                <h1>{filme.title}</h1>
+                <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+
+                <h3>Sinopse</h3>
+                <span>{filme.overview ? filme.overview : "Ainda não esta disponivel a sinopse desse filme."}</span>
+                <strong>Avaliação: {filme.vote_average.toFixed(1)} / 10</strong>
+
+                <div className="area-buttons">
+                    <button>Salvar</button>
+                    <button>
+                        <a href="#">Trailer</a>
+                    </button>
+                </div>
+            </div>
+        ) : (
+            <div className="filme-info">
+                <h1>Carregando detalhes...</h1>
+            </div>
+        )
     )
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,8 @@
-import { useState, useEffect } from "react"
+import { useState, useLayoutEffect } from "react"
 import api from "../../services/api";
+import './home.css'
+
+import { Link } from "react-router-dom";
 
 // Url da API: movie/now_playing?api_key=2f80d2c6cee2d978397b2ef6c5ba08a0&language=pt-BR
 
@@ -7,8 +10,9 @@ import api from "../../services/api";
 
 export default function Home(){
     const [filmes, setFilmes] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
+    useLayoutEffect(() => {
         async function loadFilmes(){
             const response = await api.get("/movie/now_playing",{
                 params:{
@@ -17,14 +21,30 @@ export default function Home(){
                     page: 1,
                 }
             })
-            setFilmes(response.data.results.slice(0,10))
+            setFilmes(response.data.results.slice(0,20))
+            setLoading(false)
         }
 
         loadFilmes()
     },[])
+
     return(
-        <div className="container">
-            <h1>Tela de Home</h1>
-        </div>
+        loading ? (
+            <div className="loading">
+                <h2>Carregando filmes...</h2>
+            </div>
+            ) : (
+            <div className="container">
+                <div className="lista-filmes">
+                    {filmes.map((filme, index) => (
+                        <article key={filme.id}>
+                            <strong >{filme.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+                            <Link to={`/filme/${filme.id}`}>Acessar Filme</Link>
+                        </article>
+                    ))}
+                </div>
+            </div>
+        )
     )
 }
